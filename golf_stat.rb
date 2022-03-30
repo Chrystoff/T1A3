8abf752f67d42f35148639406db845f9c73c88f7
Brian <10380@coderacademy.edu.au>
2022-03-31 10:04:54 +1100

    minor adjustments to notes. personal_score is now a clone

--- golf_stat.rb
+++ golf_stat.rb
@@ -5,8 +5,7 @@ par_count = { one: 4, two: 4, three: 5, four: 3, five: 4, six: 4, seven: 4, eigh
 high_score = { one: nil, two: nil, three: nil, four: nil, five: nil, six: nil, seven: nil, eight: nil, nine: nil,
                ten: nil, eleven: nil, twelve: nil, thirteen: nil, fourteen: nil, fifteen: nil, sixteen: nil, seventeen: nil, eighteen: nil }
 # Default scores
-personal_score = { one: nil, two: nil, three: nil, four: nil, five: nil, six: nil, seven: nil, eight: nil, nine: nil,
-                   ten: nil, eleven: nil, twelve: nil, thirteen: nil, fourteen: nil, fifteen: nil, sixteen: nil, seventeen: nil, eighteen: nil }
+personal_score = high_score.clone
 
 puts "Welcome to the golf stat tracker!"
 
@@ -14,7 +13,7 @@ puts "Welcome to the golf stat tracker!"
 def add_score(ps)
     puts "Which set of holes? (f)ront nine, (b)ack nine, or (a)ll eighteen"
     choice = gets.chomp.downcase # Getting input for menu option
-    case choice # Case method based off 'choice' variable
+    case choice # Case method based off 'choice' variable   ### TODO ONLY ADD NUMBERS
     when "f" # Front 9 holes
         puts "Enter stroke count: "
         ps.first(9).each do |k, _v|
@@ -22,7 +21,6 @@ def add_score(ps)
         end
     when "b" # Back 9 holes
         puts "Enter stroke count: "
-        # Add to hash from 10-18
         ps.each_with_index do |k, i| # going through each with index, with a to_a method to make sure I can use array methods on the hash.
             if i < 9 # I don't want the front 9 holes to be changed
                 next # if index is under 9 ie 'hole 10' then skip to next index
@@ -52,14 +50,14 @@ def menu(pc, ps, hs)
             if ps[:one].nil? && ps[:ten].nil? # if statement to check if any values exist yet
                 puts "No Personal score yet." # Checks if hash is empty and delivers this outcome
             else
-                # puts "You had a stroke count of #{ps.values.sum}" # TODO FIX ERROR CAN'T ADD NIL might have to make a incrementer thing
+                # puts "You had a stroke count of #{ps.values.sum}"     ### TODO FIX ERROR CAN'T ADD NIL might have to make a incrementer thing
                 puts "Scores: "
                 # Goes through all available personal scores
                 ps.each do |k, v|
                  p "Hole: #{k.capitalize} @ #{v}" unless v.nil?
                 end
             end
-        when "4" # Viewing the high scores # TODO NOT IMPLEMENTED
+        when "4" # Viewing the high scores      ### TODO NOT IMPLEMENTED
             puts "Best score!"
             hs.each { |k, v| p "Hole: #{k.capitalize} @ #{v}" unless v.nil? }
         when "5" # Quit
@@ -75,6 +73,17 @@ def make_highest(ps, hs)
   # if ps[key] = value > hs[key] = value
   # save into hs{}
 end
+
+# Plan to make a method that works out an average of all your scores
+def make_average(ps)
+# This needs my read and write part to be active
+end
+
+# Planning to have a feature which saves hashes to a file to recall or use presonally. 
+# also would use this in the make_average, make_highest methods
+def save_to_file(ps, hs)
+end
+
 
 # Method call and parameters
 menu(par_count, personal_score, high_score)
